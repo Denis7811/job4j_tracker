79e58f6450b9a8486789ed491340f0dd62dfa196
Denis7811 <zz926@ya.ru>
2021-10-23 09:19:48 +0300

    1.4. Вызов метода с возвращаемым типом 0919

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -9,7 +9,7 @@ public class DummyDic {
     public static void main(String[] args) {
         DummyDic petya = new DummyDic();
         String song = "I believe, I can fly";
-        String song1 = "Who let the dogs out";
+        String song1 = "Who let";
         petya.engToRus(song);
         petya.engToRus(song1);
            }
