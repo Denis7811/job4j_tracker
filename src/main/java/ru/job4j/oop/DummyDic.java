f15a34dde544586bee5e561d40b29e7ef67a00e0
Denis7811 <zz926@ya.ru>
2021-10-23 09:05:06 +0300

    1.4. Вызов метода с возвращаемым типом 0904

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -5,6 +5,7 @@ public class DummyDic {
         System.out.println("Неизвестное слово. " + eng);
     return eng;
     }
+
     public static void main(String[] args) {
         DummyDic petya = new DummyDic();
         String song = "I believe, I can fly";
