d8485fbe5a15a670073797382d7ac9577b068aa4
Denis7811 <zz926@ya.ru>
2021-10-26 07:00:58 +0300

    2.2. Рефакторинг - Площадь треугольника 1026

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,52 @@
+package ru.job4j.oop;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double semiPerimeter(double a, double b, double c) {
+        return ((a + b + c) / 2);
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        if (ab + ac > bc && ab + bc > ac && ac + bc > ab) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double area() {
+        double area = 0;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+                if (this.exist(ab, ac, bc)) {
+                    double p = semiPerimeter(ab, ac, bc);
+                    area = sqrt(p * (p - ab) * (p - ac) * (p - bc));
+                } else {
+                    System.out.println("Ошибка! Треугольник не существует!");
+                }
+            return area;
+        }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(0, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        System.out.println(rsl);
+    }
+}
+
