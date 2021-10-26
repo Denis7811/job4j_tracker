5e38a65fcf70362437ac27c7a1fe4e264b7c3a79
Denis7811 <zz926@ya.ru>
2021-10-26 05:34:30 +0300

    1.8. Состояние объекта 1026

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -7,20 +7,16 @@ public class Battery {
         this.load = size;
     }
 
-    public void exchange(Battery another, Battery another1) {
-        this.load = this.load + another.load + another1.load;
-        another.load = 0;
-        another1.load = 0;
+    public void exchange(Battery another) {
+        another.load = this.load + another.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
         Battery batDur = new Battery(71);
         Battery batEner = new Battery(33);
-        Battery batVolna = new Battery(57);
-        System.out.println("Durac : " + batDur.load + ". Energaiz : " + batEner.load + ". Volna : "
-                + batVolna.load);
-        batVolna.exchange(batDur, batEner);
-        System.out.println("Durac : " + batDur.load + ". Energaiz : " + batEner.load + ". Volna : "
-                + batVolna.load);
+        System.out.println("Duracell : " + batDur.load + ". Energaizer : " + batEner.load);
+        batDur.exchange(batEner);
+        System.out.println("Duracell : " + batDur.load + ". Energaizer : " + batEner.load);
         }
 }
