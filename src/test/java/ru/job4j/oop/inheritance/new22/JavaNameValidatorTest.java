47b1469cc6a7407fa1065a1a90dcfc86da99b8f3
Denis7811 <zz926@ya.ru>
2022-09-13 12:00:53 +0300

    6.9. String - это массив [#504884 #334425]

--- src/test/java/ru/job4j/oop/inheritance/new22/JavaNameValidatorTest.java
+++ src/test/java/ru/job4j/oop/inheritance/new22/JavaNameValidatorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 import static ru.job4j.oop.inheritance.new22.JavaNameValidator.*;
 
-
 class JavaNameValidatorTest {
 
     @Test
