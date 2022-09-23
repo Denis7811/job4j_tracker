2a2cb8e718101db7ca76c808b02cdbb36a49ecb4
Denis7811 <zz926@ya.ru>
2022-09-23 15:17:26 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,24 +57,4 @@ public class Tracker {
         }
         return rsl;
     }
-
-    public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        Item first = new Item(10, "First");
-        Item second = new Item("Second2");
-        Item second2 = new Item();
-        Item second3 = new Item(1, "Second2");
-        Item second4 = new Item("Second2");
-        tracker.add(first);
-        tracker.add(second);
-        tracker.add(second2);
-        tracker.add(second3);
-        tracker.add(second4);
-        System.out.println(tracker.indexOf(4));
-        /*Item[] rsl = tracker.findAll();
-        for (int index = 0; index < rsl.length; index++) {
-            Item item = rsl[index];
-            System.out.println(item.getId() + " " + item.getName());
-        }*/
-    }
 }
