09ed15f67cd8c327b35645780cb7ed188da046b5
Denis7811 <zz926@ya.ru>
2022-10-21 09:50:58 +0300

    3.2. Зачем нужно приведение типов [#310019 #351580]

--- src/main/java/ru/job4j/oop/poly/Bus.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.oop.poly;
-
-public class Bus  implements Vechicle {
-    @Override
-    public void move() {
-        System.out.println(getClass().getSimpleName()
-                + " - автобус двигается по скоростным трассам");
-    }
-}
