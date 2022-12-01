6a5167427e3266c45a8be62783aecd9aae885076
Denis7811 <zz926@ya.ru>
2022-12-01 15:08:35 +0300

    0.5. Иерархия исключений и множественный catch.

--- src/main/java/ru/job4j/excep/UserStore.java
+++ src/main/java/ru/job4j/excep/UserStore.java
@@ -2,39 +2,39 @@ package ru.job4j.excep;
 
 public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        int rsl = -1;
-        for (User u : users) {
-            if (login.equals(u.getUsername())) {
-                rsl = 1;
+        User temp = null;
+        for (User user : users) {
+            if (login.equals(user.getUsername())) {
+                temp = user;
                 break;
+            } else {
+                throw new UserNotFoundException("User not found");
             }
         }
-        if (rsl == -1) {
-            throw new UserNotFoundException("User not found");
-        }
-        return null;
+            return temp;
     }
 
-    public static boolean validate(User user) throws UserInvalidException {
-        if (!user.isValid() || user.getUsername().length() < 3) {
-            throw new UserInvalidException("User not Valid");
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        User[] users = {
-                new User("Petr Arsentev", true)
-        };
-        try {
-            User user = findUser(users, "Petr Arsentev");
-            if (validate(user)) {
-                System.out.println("This user has an access");
+        public static boolean validate(User user) throws UserInvalidException {
+            if (!user.isValid() || user.getUsername().length() < 3) {
+                throw new UserInvalidException("User not Valid");
             }
-        } catch (UserInvalidException ui) {
-            ui.printStackTrace();
-        } catch (UserNotFoundException un) {
+            return true;
+        }
+
+        public static void main(String[]args) {
+            User[] users = {
+                    new User("Petr Arsentev", true)
+            };
+            try {
+                User user = findUser(users, "Petr Arsentev");
+                if (validate(user)) {
+                    System.out.println("This user has an access");
+                }
+            } catch (UserInvalidException ui) {
+                ui.printStackTrace();
+            } catch (UserNotFoundException un) {
                 un.printStackTrace();
+            }
         }
-    }
-}
+   }
+
