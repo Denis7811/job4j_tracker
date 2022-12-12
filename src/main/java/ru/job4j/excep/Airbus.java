79c4ca8661e05f2faa5400b8245eec1b4ff5b4d0
Denis7811 <zz926@ya.ru>
2022-12-12 11:59:26 +0300

    3. final [#334073]

---
+++ src/main/java/ru/job4j/excep/Airbus.java
@@ -0,0 +1,36 @@
+package ru.job4j.excep;
+
+public final class Airbus extends Aircraft {
+   private static int count_engine = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        count_engine = name.equals("A380") ? 4 : 2;
+        System.out.println("Количество двигателей равно: " + count_engine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
