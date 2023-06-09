919a0dc72f940404a4d3a22ce28b55cc757d98f0
Denis7811 <zz926@ya.ru>
2023-06-09 07:29:10 +0300

    0. Map, HashMap [#212652 # [#212652 #421727]]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -8,6 +8,8 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("zz1@gmail.com", "Denis Alex Zotin");
         map.put("zz2@gmail.com", "Alex Mih Petrov");
+        map.put("zz1@gmail.com", "Denis Vlad Vedv");
+        map.put("zz2@gmail.com", "Mih Kutuz Grinev");
         map.put("zz3@gmail.com", "Ivan Andreev Krilov");
         for (String key : map.keySet()) {
             String value = map.get(key);
