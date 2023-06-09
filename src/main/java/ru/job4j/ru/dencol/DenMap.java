0af7ce5fc9569e9950bd2aaf5358d8642f472d88
Denis7811 <zz926@ya.ru>
2023-06-09 07:34:29 +0300

    0609

--- src/main/java/ru/job4j/ru/dencol/DenMap.java
+++ src/main/java/ru/job4j/ru/dencol/DenMap.java
@@ -8,12 +8,14 @@ public class DenMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
         map.put("zz@yandex.ru", "Denis");
+        String value1 = map.get("zz@yandex.ru");
+        System.out.println(value1);
         map.remove("parsentev@yandex.ru");
-        /*for (String key : map.keySet()) {
-           // String value = map.get(key);
-            //System.out.println(key + " = " + value);
+        for (String key : map.keySet()) {
+           String value = map.get(key);
+            System.out.println(key + " = " + value);
             System.out.println(key);
-        }*/
+        }
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
