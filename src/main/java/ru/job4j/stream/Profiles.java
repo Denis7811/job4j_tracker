cc3621dc01eafa1571c45aad7538764dc291eb6a
Denis7811 <zz926@ya.ru>
2024-09-06 07:59:42 +0300

    6. Список адресов [#110062 #511972]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    public static List<Address> collect(List<Profile> profile) {
-        return profile.stream().map(Profile::getAddress)
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream().map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 }
